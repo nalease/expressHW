8086e0edd0edf9cf4530542178f7a6590280f289
Nalease Marrow <Naleasem@Naleases-MacBook-Pro.local>
2018-04-07 12:30:53 -0400

    Add to friend.js

---
+++ FriendFinder/data/friends.js
@@ -0,0 +1,34 @@
+var friends = [{
+    "name": "Fine man",
+    "photo": "https://i.pinimg.com/564x/05/12/af/0512af6bcb798ea37cbb46cec1029a82.jpg",
+    "scores": [5, 2, 3, 5, 5, 3, 2, 5, 1, 1]
+},
+{
+    "name": "Fine man #1",
+    "photo": "https://i.pinimg.com/564x/ca/59/42/ca5942ed0455c65f530baeffb6eefe27.jpg",
+    "scores": [3, 2, 4, 1, 3, 4, 2, 3, 4, 2]
+},
+{
+    "name": "Fine man #2",
+    "photo": "https://i.pinimg.com/564x/e4/88/ab/e488ab244c2477d1a91ee5a1dee8ab91.jpg",
+    "scores": [1, 2, 4, 3, 5, 4, 2, 5, 2, 3]
+},
+{
+    "name": "Fine man #3",
+    "photo": "https://i.pinimg.com/564x/41/68/ca/4168ca6adf2a99c5a52e62edc62d78e8.jpg",
+    "scores": [2, 5, 2, 1, 5, 2, 2, 5, 3, 1]
+},
+{
+    "name": "Fine man #4",
+    "photo": "https://i.pinimg.com/564x/4f/56/6d/4f566d0e4f9160f3f559c1bf45479a85.jpg",
+    "scores": [2, 5, 2, 4, 1, 1, 2, 2, 4, 4]
+},
+{
+    "name": "Fine man #5",
+    "photo": "https://i.pinimg.com/736x/10/08/94/1008944a5bf74a24393e8cf0ebf75770--charles-michael-davis-gorgeous-men.jpg",
+    "scores": [3, 4, 2, 4, 3, 2, 2, 2, 4, 3]
+},
+]
+
+
+module.exports = friends;
